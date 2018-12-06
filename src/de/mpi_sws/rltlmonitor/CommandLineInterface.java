e150d7c09adf34e7653f69adc51a5df44769f9e2
Daniel Neider <neider@mpi-sws.org>
2018-12-06 17:51:48 +0100

    Added Leucker et al.'s LTL monitor construction

--- src/de/mpi_sws/rltlmonitor/CommandLineInterface.java
+++ src/de/mpi_sws/rltlmonitor/CommandLineInterface.java
@@ -12,6 +12,8 @@ import org.mpi_sws.rltl.visitors.RLTL2LTLVisitor;
 
 import net.automatalib.automata.transout.impl.FastMoore;
 import net.automatalib.util.automata.Automata;
+import owl.automaton.AutomatonUtil;
+import owl.ltl.parser.LtlParser;
 import owl.run.DefaultEnvironment;
 import owl.translations.LTL2DAFunction;
 
@@ -38,20 +40,111 @@ public class CommandLineInterface {
 		//
 		// Do the monitor construction
 		//
-		try {
+//		try {
+//
+//			var monitor = constructrLTLMonitor(args[0]);
+//
+//			System.out.println("\n========== Final Monitor ==========\n");
+//			System.out.println(Owl2Automatalib.toDot(monitor));
+//
+//		} catch (UnsupportedEncodingException | ParseException e) {
+//			e.printStackTrace();
+//		}
 
-			var monitor = constructrLTLMonitor(args[0]);
+		var monitor = constructLTLMonitor(args[0]);
 
-			System.out.println("\n========== Final Monitor ==========\n");
-			System.out.println(Owl2Automatalib.toDot(monitor));
-
-		} catch (UnsupportedEncodingException | ParseException e) {
-			e.printStackTrace();
-		}
+		System.out.println("\n========== Final Monitor ==========\n");
+		System.out.println(Owl2Automatalib.toDot(monitor));
 
 	}
 
-	private static FastMoore<BitSet, BitSet> constructrLTLMonitor(String rLTLFormula)
+	public static FastMoore<BitSet, BitSet> constructLTLMonitor(String ltlFormula) {
+
+		//
+		// Negate LTL formula
+		//
+		String negatedLTLFormula = "!(" + ltlFormula + ")";
+
+		//
+		// Owl automaton for negated formula
+		//
+		var translator = new LTL2DAFunction(DefaultEnvironment.standard(), false,
+				EnumSet.of(LTL2DAFunction.Constructions.PARITY));
+
+		var negatedOwlAutomaton = AutomatonUtil.cast(translator.apply(LtlParser.parse(negatedLTLFormula)));
+		System.out.println("Negated formula: " + negatedLTLFormula);
+		System.out.println("\n---------- Start Owl automaton ----------");
+		System.out.println(owl.automaton.output.HoaPrinter.toString(negatedOwlAutomaton));
+		System.out.print("Alphabet: ");
+		for (int i = 0; i < negatedOwlAutomaton.factory().alphabet().size(); ++i) {
+			System.out.print((i == 0 ? "" : "; ") + negatedOwlAutomaton.factory().alphabet().get(i) + " (" + i + ")");
+		}
+		System.out.println("\n---------- End Owl automaton ----------");
+
+		//
+		// Convert to Moore machine
+		//
+		var negatedMachine = Owl2Automatalib.toAutomatalib(negatedOwlAutomaton, 0);
+		System.out.println("\n---------- Start Moore machine ----------");
+		System.out.println(Owl2Automatalib.toDot(negatedMachine));
+		System.out.println("---------- End Moore machine ----------");
+
+		//
+		// Minimize Moore machine
+		//
+		Automata.invasiveMinimize(negatedMachine, negatedMachine.getInputAlphabet());
+		System.out.println("\n---------- Start minimized Moore machine ----------");
+		System.out.println(Owl2Automatalib.toDot(negatedMachine));
+		System.out.println("---------- End minimized Moore machine ----------");
+
+		//
+		// Owl automaton for original formula
+		//
+		var originalOwlAutomaton = AutomatonUtil.cast(translator.apply(LtlParser.parse(ltlFormula)));
+		System.out.println("Original formula: " + ltlFormula);
+		System.out.println("\n---------- Start Owl automaton ----------");
+		System.out.println(owl.automaton.output.HoaPrinter.toString(originalOwlAutomaton));
+		System.out.print("Alphabet: ");
+		for (int i = 0; i < originalOwlAutomaton.factory().alphabet().size(); ++i) {
+			System.out.print((i == 0 ? "" : "; ") + originalOwlAutomaton.factory().alphabet().get(i) + " (" + i + ")");
+		}
+		System.out.println("\n---------- End Owl automaton ----------");
+
+		//
+		// Convert to Moore machine
+		//
+		var originalMachine = Owl2Automatalib.toAutomatalib(originalOwlAutomaton, 1);
+		System.out.println("\n---------- Start Moore machine ----------");
+		System.out.println(Owl2Automatalib.toDot(originalMachine));
+		System.out.println("---------- End Moore machine ----------");
+
+		//
+		// Minimize Moore machine
+		//
+		Automata.invasiveMinimize(originalMachine, originalMachine.getInputAlphabet());
+		System.out.println("\n---------- Start minimized Moore machine ----------");
+		System.out.println(Owl2Automatalib.toDot(originalMachine));
+		System.out.println("---------- End minimized Moore machine ----------");
+
+		//
+		// Compute product
+		//
+		var combinedMachine = Owl2Automatalib.naiveProduct(negatedMachine, originalMachine);
+		System.out.println("\n---------- Start combined Moore machine ----------");
+		System.out.println(Owl2Automatalib.toDot(combinedMachine));
+		System.out.println("---------- End combined Moore machine ----------");
+
+		// Minimize
+		Automata.invasiveMinimize(combinedMachine, combinedMachine.getInputAlphabet());
+		System.out.println("\n---------- Start minimized combined Moore machine ----------");
+		System.out.println(Owl2Automatalib.toDot(combinedMachine));
+		System.out.println("---------- End minimized combined Moore machine ----------");
+
+		return combinedMachine;
+
+	}
+
+	public static FastMoore<BitSet, BitSet> constructrLTLMonitor(String rLTLFormula)
 			throws ParseException, UnsupportedEncodingException {
 
 		//
@@ -70,7 +163,7 @@ public class CommandLineInterface {
 		//
 		// Construct monitor
 		//
-		FastMoore<BitSet, BitSet> combined = null;
+		FastMoore<BitSet, BitSet> combinedMachine = null;
 		for (int bit = 0; bit < 4; ++bit) {
 
 			System.out.println("\n========== (bit=" + bit + ") ==========\n");
@@ -119,7 +212,7 @@ public class CommandLineInterface {
 			// In first iteration, just store AutomataLib Moore machine
 			//
 			if (bit == 0) {
-				combined = automatalibMachine;
+				combinedMachine = automatalibMachine;
 			}
 
 			//
@@ -128,22 +221,22 @@ public class CommandLineInterface {
 			else {
 
 				// Combine
-				combined = Owl2Automatalib.naiveProduct(combined, automatalibMachine);
+				combinedMachine = Owl2Automatalib.naiveProduct(combinedMachine, automatalibMachine);
 				System.out.println("\n---------- Start combined Moore machine ----------");
-				System.out.println(Owl2Automatalib.toDot(combined));
+				System.out.println(Owl2Automatalib.toDot(combinedMachine));
 				System.out.println("---------- End combined Moore machine ----------");
 
 				// Minimize
-				Automata.invasiveMinimize(combined, combined.getInputAlphabet());
+				Automata.invasiveMinimize(combinedMachine, combinedMachine.getInputAlphabet());
 				System.out.println("\n---------- Start minimized combined Moore machine ----------");
-				System.out.println(Owl2Automatalib.toDot(combined));
+				System.out.println(Owl2Automatalib.toDot(combinedMachine));
 				System.out.println("---------- End minimized combined Moore machine ----------");
 
 			}
 
 		}
 
-		return combined;
+		return combinedMachine;
 
 	}
 
